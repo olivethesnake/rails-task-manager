844077394e7eddb137105effc0344cc1e6e789ad
olivia franklin <frolivia@yahoo.fr>
2023-02-21 18:02:16 +0100

    jghh

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   # Defines the root path route ('/')
+#   # root 'articles#index'
+#   get 'tasks', to: 'tasks#index'
+#   get 'tasks/:id', to: 'tasks#show', as: :task
+#   get 'tasks/new', to: 'tasks#new', as:  :new_track
+#   post 'tasks', to: 'tasks#create'
+#   get 'tasks/:id/edit', to: 'tasks#edit'
+#   patch 'tasks/:id', to: 'tasks#update'
+#   delete 'tasks/:id', to: 'tasks#destroy'
+#   # resources :tasks, only: [:create, :index, :destroy, :new, :show,:edit, :update,]
+# end
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
